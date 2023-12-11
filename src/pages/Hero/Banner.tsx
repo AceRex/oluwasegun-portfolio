2fb4cc2c961228b6df48b116d2c303b450387155
AceRex <johnsonare2207@gmail.com>
2023-12-11 09:01:18 +0100

    pushit

--- src/pages/Hero/Banner.tsx
+++ src/pages/Hero/Banner.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 function Banner() {
-  return <div className='p-12'>
-    <span>Hi there,</span>
-  </div>;
+  return (
+    <div className="p-12 flex">
+      <div className="w-[45%]">
+        <span className="uppercase font-semibold dark:text-ar_color2 text-ar_neutral text-sm bg-ar_color4 rounded-md p-3">
+          💡 software developer | Creative Designer
+        </span>
+        <p className="dark:text-ar_neutral text-ar_color2 text-7xl font-bold pt-5 leading-tight">
+          <TypeAnimation
+            sequence={[
+              `Hi there 👋🏾,\nI am Are Oluwasegun Johnson...`,
+              1000,
+              "But please call me Rex.",
+              1000,
+            ]}
+            speed={50}
+            repeat={Infinity}
+          />
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Banner;
