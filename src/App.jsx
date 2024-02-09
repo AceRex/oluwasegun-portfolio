6f230240a857ca9afe3ba33b80ddeb309fa9a824
AceRex <johnsonare2207@gmail.com>
2024-02-09 16:19:02 +0100

    About me

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <div id="reader" className="container m-auto">
       <Hero />
-      {/* <AboutMe /> */}
+      <AboutMe />
    
     </div>
   );
--- src/pages/Hero/Banner.tsx
+++ src/pages/Hero/Banner.tsx
@@ -5,12 +5,12 @@ import ProfileImage from "../../assets/Web.png";
 
 function Banner() {
   return (
-    <div className="p-12 flex content-center">
-      <div className="w-[45%]">
-        <span className="uppercase font-semibold dark:text-ar_color2 text-ar_neutral text-sm bg-ar_color4 rounded-md p-3">
+    <div className="p-12 w-[90%] h-[75vh] m-auto flex content-center">
+      <div className="w-[60%] m-auto mt-5 text-center">
+        <span className="uppercase h-[20vh] font-semibold dark:text-ar_color2 text-ar_neutral text-sm bg-ar_color4 rounded-md p-3">
           💡 software developer | Creative Designer
         </span>
-        <p className="dark:text-ar_neutral text-ar_color2 text-7xl font-bold pt-5 leading-tight">
+        <p className="dark:text-ar_neutral h-[60vh] text-ar_color2 text-7xl font-bold pt-5 leading-tight">
           <TypeAnimation
             sequence={[
               `Hi there 👋🏾,\nI am Are Oluwasegun Johnson...`,
@@ -23,9 +23,9 @@ function Banner() {
           />
         </p>
       </div>
-      <div className="w-[45%]">
+      {/* <div className="w-[45%]">
         <img src={ProfileImage} className="w-25" />
-      </div>
+      </div> */}
     </div>
   );
 }
