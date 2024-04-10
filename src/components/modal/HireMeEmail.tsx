61ac08d45e8ac41c1cc751b26b9128ab66060835
AceRex <johnsonare2207@gmail.com>
2024-04-10 11:20:18 +0100

    push

--- src/components/modal/HireMeEmail.tsx
+++ src/components/modal/HireMeEmail.tsx
@@ -37,17 +37,17 @@ export default function HireMe() {
   };
 
   return (
-    <div className="absolute top-0 left-0 dark:bg-ar_color1/20 bg-ar_color1/40 backdrop-blur-lg z-0 h-screen w-screen p-12">
-      <form className="dark:bg-ar_color1 bg-ar_neutral_alpha w-[50%] m-auto p-24 text-center rounded-xl">
+    <div className="absolute max-md:fixed top-0 left-0 dark:bg-ar_color1/20 bg-ar_color1/40 backdrop-blur-lg z-0 h-screen w-screen max-sm:min-h-screen p-12 max-sm:p-2">
+      <form className="dark:bg-ar_color1 bg-ar_neutral_alpha w-[50%] max-sm:w-[100%] m-auto p-24 max-sm:p-5 max-sm:py-10 text-center rounded-xl">
         <div className="mb-6">
-          <p className="font-extrabold text-4xl text-ar_color4 dark:text-ar_color4">Hire Me!</p>
-          <p>
+          <p className="font-extrabold text-4xl max-sm:text-3xl text-ar_color4 dark:text-ar_color4">Hire Me!</p>
+          <p className="max-sm:text-sm">
             Thank you for your interest in me, kindly fill the form below and I
             will reach out to you as soon as you click the send button
           </p>
         </div>
         <div className="flex flex-col text-left gap-2 mb-4">
-          <label className="font-semibold ml-1 text-ar_neutral">Name</label>
+          <label className="font-semibold ml-1  dark:text-ar_neutral text-ar_color1">Name</label>
           <input
             value={hireMeName}
             type="text"
@@ -57,7 +57,7 @@ export default function HireMe() {
           />
         </div>
         <div className="flex flex-col text-left gap-2 mb-4">
-          <label className="font-semibold ml-1 text-ar_neutral">Email</label>
+          <label className="font-semibold ml-1 dark:text-ar_neutral text-ar_color1">Email</label>
           <input
             value={email}
             type="email"
@@ -67,7 +67,7 @@ export default function HireMe() {
           />
         </div>
         <div className="flex flex-col text-left gap-2 mb-4">
-          <label className="font-semibold ml-1 text-ar_neutral">Message</label>
+          <label className="font-semibold ml-1 dark:text-ar_neutral text-ar_color1">Message</label>
           <textarea
             value={message}
             name="message"
@@ -77,11 +77,11 @@ export default function HireMe() {
             {" "}
           </textarea>
         </div>
-        <div className="flex flex-row gap-2">
+        <div className="flex flex-row max-sm:flex-col-reverse gap-2">
           <button
             type="submit"
             onClick={handleClose}
-            className="p-3 rounded-lg border border-ar_color4 dark:border-ar_neutral_alpha w-[70%] dark:text-ar_neutral_alpha text-ar_color4 dark:text-ar_color1"
+            className="p-3 rounded-lg border border-ar_color4 dark:border-ar_neutral_alpha w-[70%] max-sm:w-[100%] dark:text-ar_neutral_alpha text-ar_color4 dark:text-ar_color1"
           >
             {" "}
             Close{" "}
@@ -89,7 +89,7 @@ export default function HireMe() {
           <button
             type="submit"
             onClick={sendEmail}
-            className="p-3 rounded-lg bg-ar_color2 dark:bg-ar_color4 w-[70%] text-ar_neutral dark:text-ar_color1"
+            className="p-3 rounded-lg bg-ar_color2 dark:bg-ar_color4 w-[70%] max-sm:w-[100%] text-ar_neutral dark:text-ar_color1"
           >
             {" "}
             Send{" "}
