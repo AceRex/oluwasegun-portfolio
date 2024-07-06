ecbeea4097356b56948534385eeb79796d8f43d7
AceRex <johnsonare2207@gmail.com>
2024-07-06 09:15:20 +0100

    ##Updated

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,6 +10,12 @@ const Routers = () => {
         <Route path="/" element={<App />}>
           <Route path="" element={<MainView />} />
           <Route path="about-me" element={<AboutUs />} />
+          <Route path="my-skill" element={<h1>Page is not done</h1>} />
+          <Route path="my-project" element={<h1>Page is not done</h1>} />
+          <Route path="work-experience" element={<h1>Page is not done</h1>} />
+          <Route path="education" element={<h1>Page is not done</h1>} />
+          <Route path="my-contact" element={<h1>Page is not done</h1>} />
+          <Route path="leave-a-comment" element={<h1>Page is not done</h1>} />
         </Route>
       </Routes>
     </Router>
